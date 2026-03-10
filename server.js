7ddc9f9388bdf05129c636638127e8773554781b
niyazi <niyazionderr@gmail.com>
2026-03-10 11:26:53 +0300

    Serve index.html from root directory

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const io = new Server(server, {
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
